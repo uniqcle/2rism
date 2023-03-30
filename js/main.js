91ff998faa0910a35dcb4e71e2dd552bb8177fe2
uniqcle <uniqcle@yandex.ru>
2023-03-30 12:51:30 +0700

    added datepick plugin

--- js/main.js
+++ js/main.js
@@ -1,2 +1,22 @@
 customSelect('select');
 
+/* datepicker */
+
+const picker = new easepick.create({
+    element: "#when",
+    css: [
+        "https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css"
+    ],
+    zIndex: 10,
+    lang: "ru-RU",
+    grid: 2,
+    format: 'DD.MM.YY',
+    calendars: 2,
+    plugins: [
+        "RangePlugin",
+        "TimePlugin"
+    ]
+});
+
+
+
