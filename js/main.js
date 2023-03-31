61356f44d31914bc1904a51e89e1f5c330ff2cd8
uniqcle <uniqcle@yandex.ru>
2023-03-31 15:36:03 +0700

    mobile adaptive

--- js/main.js
+++ js/main.js
@@ -18,5 +18,18 @@ const picker = new easepick.create({
     ]
 });
 
+// Subscribe label 
+
+const inputSubscribe = document.querySelector('.subscribe__input');
+const label = document.querySelector('.subscribe__label');
+
+inputSubscribe.addEventListener('input', () => {
+    if (!inputSubscribe.value.trim()) {
+        label.classList.add('subscribe__label--top')
+    } else {
+        label.classList.remove('subscribe__label--top');
+    }
+})
+
 
 
